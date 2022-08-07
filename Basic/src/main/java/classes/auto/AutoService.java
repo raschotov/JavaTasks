06333716d3f960f1e956049c2f4c0add748291d3
Nikita <nik.nesterenko1999@bk.ru>
2022-08-07 22:10:54 +0300

    Задачи. Классы, объекты, строки, операции над ними.

---
+++ Basic/src/main/java/classes/auto/AutoService.java
@@ -0,0 +1,35 @@
+package classes.auto;
+
+/**
+ * Сервис по работе с автомобилями
+ */
+public class AutoService {
+
+    /**
+     * Данный метод возвращает автомобили марки BMW
+     * @param autos все автомобили
+     * @return массив автомобилей БМВ.
+     */
+    public Auto[] getOnlyBmwCars(Auto[] autos) {
+        return null;
+    }
+
+    /**
+     * Принято считать что если автомобиль двух-местный, то он из спорт-класса
+     * Данный метод возвращает автомобили спорт-класса
+     * @param autos все автомобили
+     * @return автомобили спорт-класса
+     */
+    public Auto[] getSportCars(Auto[] autos) {
+        return null;
+    }
+
+    /**
+     * Данный метод возвращает автомобили только с автоматической коробкой передач
+     * @param autos все автомобили
+     * @return автомобили с автоматической коробкой передач.
+     */
+    public Auto[] getOnlyAutomaticTypeCars(Auto[] autos) {
+        return null;
+    }
+}
