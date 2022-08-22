4ebf2e1ce4c984d4ebc1ffbd2a3d4f5d3fda4899
raschotov <evg.raschotov@gmail.com>
2022-08-22 22:32:46 +0300

    29.07 Homework: 2 tasks done, unnecessary class PointService removed

--- OOP/src/main/java/task/point/Point.java
+++ OOP/src/main/java/task/point/Point.java
@@ -16,4 +16,8 @@ public class Point {
 	public int getCoordinateY(){
 		return this.coordinateY;
 	}
+
+	public static double distance(Point point1, Point point2) {
+		return Math.sqrt(Math.pow(point2.getCoordinateX() - point1.getCoordinateX(), 2) + Math.pow(point2.getCoordinateY() - point1.getCoordinateY(), 2));
+	}
 }
--- OOP/src/test/java/task/point/PointTest.java
+++ OOP/src/test/java/task/point/PointTest.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class PointServiceTest {
+public class PointTest {
 
 	@Test
 	public void distance() {
 		Point point1 = new Point(15,16);
 		Point point2 = new Point(23, 42);
-		double result = PointService.distance(point1, point2);
+		double result = Point.distance(point1, point2);
 
 		//'assertEquals(double, double)' is deprecated, "fuzz factor" value needed
 		Assert.assertEquals(27.202941017470888, result, 0.001);
